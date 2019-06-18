aa12932dc575d08d1a1f139f1be816f59d03e778
Josep M. Blanquer <blanquer@gmail.com>
2019-06-18 15:38:14 -0700

    quick pass at removing Praxis::Links (and link builder) from mediatypes and such

--- lib/praxis.rb
+++ lib/praxis.rb
@@ -50,7 +50,6 @@ module Praxis
   autoload :RestfulDocGenerator, 'praxis/restful_doc_generator'
   module Docs
     autoload :Generator, 'praxis/docs/generator'
-    autoload :LinkBuilder, 'praxis/docs/link_builder'
   end
 
   # types
@@ -60,7 +59,6 @@ module Praxis
     autoload :MultipartArray, 'praxis/types/multipart_array'
   end
 
-  autoload :Links, 'praxis/links'
   autoload :MediaType, 'praxis/media_type'
   autoload :MediaTypeCollection, 'praxis/media_type_collection'
   autoload :MediaTypeIdentifier, 'praxis/media_type_identifier'
